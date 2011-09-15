18cc49bab539a534f7331f64f61fa619835711eb
Thiago Genez <thiagogenez@gmail.com>
2011-09-15 02:14:42 -0300

    refatoring merge-sort.py

--- chapter2/python/merge-sort.py
+++ chapter2/python/merge-sort.py
@@ -1,43 +1,32 @@
 #! /usr/bin/python
 # coding: utf-8
 
-
-import sys
 import utils
 
-def merge(A,p,q,r):
-	left = list()
-	right = list()
-
-	for i in range(q):
-		left.append(A[i])
-	for i in range(q,r):
-		right.append(A[i])
-	
-	left.append(sys.maxint)
-	right.append(sys.maxint)
-	
-	a = 0
-	b = 0
-
-	for i in range(r):
-		if left[a] <= right[b]:
-			A[i] = left[a]
-			a += 1
+def merge(left, right):
+	A = []
+        i ,j = 0, 0
+	while i < len(left) and j < len(right):
+		if left[i] <= right[j]:
+			A.append(left[i])
+			i += 1
 		else:
-			A[i] = right[b]
-			b += 1
-	
+			A.append(right[j])
+			j += 1
+	A += left[i:]
+	A += right[j:]
+	return A
 
-def merge_sort(A,p,r):
-	if p < r:
-		q = (p + r) // 2
-		merge_sort(A,p,q)
-		merge_sort(A,q+1,r)
-		merge(A,p,q,r)
+
+def mergesort(A):
+	if len(A) > 1:
+		q = len(A) // 2
+		left = mergesort(A[:q])
+		right = mergesort(A[q:])
+		return merge(left, right)
+	return A
 
 if __name__ == "__main__":
 	A = utils.get_array()
 	print "unsorted(A): " + str(A)
-	merge_sort(A,0,len(A))
-	print "merge-sort(A): " + str(A)
+	print "mergesort(A): " + str(mergesort(A))
